196a8480febde59ed6f76ce598f9cab66e6486db
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2022-02-28 13:41:20 -0800

    wap, unpackaged (proj ref)

---
+++ WinUICpp_WAP/WinUICpp_WAP/MainWindow.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "MainWindow.g.h"
+
+namespace winrt::WinUICpp_WAP::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::WinUICpp_WAP::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
